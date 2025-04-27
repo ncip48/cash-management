8f033e14b7f063d2900e1193fbf808d7a000a195
herlyplanviu <herly@planviu.com>
2025-04-27 09:35:14 +0700

    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         // if (env(key: 'APP_ENV') == 'local') {
-        // URL::forceScheme(scheme: 'https');
+        URL::forceScheme(scheme: 'https');
         // }
 
         Blueprint::macro('creatorAndUpdater', function () {
